da1c33d6c09fd409c2e4d10bc14e22381d6466f2
NICOLLAS PEREIRA REZENDE <nicollaspereirarezende@outlook.com.br>
2023-11-01 15:08:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from config_bot import (VERIFY_TOKEN,
                         BASE_URL,
                         PAGE_ID,
                         saudacoes_portugues)
-from menssages import Menus, MenuOptions
+from menssages import Menus, MenuOptions, codigo_aleatorio
 from funcs import registrar_mensagem
-import time
-
-
-
 
 
 app = Flask(__name__)
@@ -27,10 +23,10 @@ def verify_webhook():
 def receive_messages():
     data = request.get_json()
 
-    numero_suporte = "5561991769500"
+    numero_para_enviar = "5561991769500"
     numero_comercial = "5561991769500"
     detalhes = None
-    palavra_chave = '55374'
+    palavra_chave = codigo_aleatorio
     text = None
     name = None
     numero_user = None
@@ -41,7 +37,8 @@ def receive_messages():
     button_reply_id = None
     list_reply_id = None
     menu_atual = None
-    opcao_menu = None
+    global opcao_menu
+    global equipe
     global aguardando_resposta
     aguardando_resposta = False
     global mensagem_usuario
@@ -101,6 +98,92 @@ def receive_messages():
                 Menus.menu_comercial_locacao(wa_id, nome)
                 registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
 
+# -------------------VOZ-------------------
+
+            if list_reply_id == 'SIP_SERVER':
+                opcao_menu = 'SIP SERVER'
+                Menus.enviar_msg_SipServer(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'GRAVADOR_CHAMADAS':
+                opcao_menu = 'GRAVADOR CHAMADAS'
+                Menus.enviar_msg_Gravador(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'APARELHOS_IP':
+                opcao_menu = 'APARELHOS IP'
+                Menus.enviar_msg_AparelhoIP(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'URA':
+                opcao_menu = 'URA'
+                Menus.enviar_msg_URA(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'GATEWAYS':
+                opcao_menu = 'GATEWAYS'
+                Menus.enviar_msg_Gateways(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'CORREIO_DE_VOZ':
+                opcao_menu = 'CORREIO DE VOZ'
+                Menus.enviar_msg_Correio_de_voz(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'RELATORIOS':
+                opcao_menu = 'RELATORIOS'
+                Menus.enviar_msg_Relatorios(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+# -------------------DADOS-------------------
+
+            if list_reply_id == 'VPN':
+                opcao_menu = 'VPN'
+                Menus.enviar_msg_VPN(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'FIREWALL':
+                opcao_menu = 'FIREWALL'
+                Menus.enviar_msg_Firewall(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'VIRTUALIZACAO':
+                opcao_menu = 'Virtualização'
+                Menus.enviar_msg_Virtualizacao(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+# -------------------SEGURANCA-------------------
+
+            if list_reply_id == 'CFTV':
+                opcao_menu = 'CFTV'
+                Menus.enviar_msg_CFTV(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'CERCA_ELETRICA':
+                opcao_menu = 'CERCA ELETRICA'
+                Menus.enviar_msg_Central_de_Cerca_Eletrica(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+            if list_reply_id == 'ALARME':
+                opcao_menu = 'ALARME'
+                Menus.enviar_msg_Central_de_Alarme(wa_id)
+                Menus.enviar_msg_exemplo(wa_id)
+                registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+# -------------------CABEAMENTO-------------------
+
             if list_reply_id == 'INFO':
                 opcao_menu = 'INFO'
                 Menus.enviar_contato(wa_id, "Comercial", numero_comercial)
@@ -156,6 +239,7 @@ def receive_messages():
         # SUPORTE TECNICO
 
         if button_reply_id == 'SUPORTE_TECNICO':
+            equipe = 'Suporte'
             opcao_menu = 'suporte1'
             Menus.menu_supote_tecnico(wa_id, nome)
             registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
@@ -177,16 +261,34 @@ def receive_messages():
         # FINANCEIRO
 
         if button_reply_id == 'FINANCEIRO':
+            equipe = 'Financeiro'
             opcao_menu = 'financeiro'
             Menus.menu_voltar(wa_id, nome)
-            Menus.menu_financeiro(wa_id, nome)
+            Menus.menu_financeiro2(wa_id, nome)
             registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
 
         # COMERCIAL
 
         if button_reply_id == 'COMERCIAL':
+            equipe = 'Comercial'
             opcao_menu = 'comercial'
             Menus.menu_comercial(wa_id, nome)
+            registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+# -------------------LOCACAO-------------------
+
+        if button_reply_id == 'LOCACAO2':
+            opcao_menu = 'Locação'
+            Menus.enviar_msg_Locação_de_equipamentos(wa_id)
+            Menus.enviar_msg_exemplo(wa_id)
+            registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
+
+# -------------------CABEAMENTO-------------------
+
+        if button_reply_id == 'CABEAMENTO':
+            opcao_menu = 'Cabeamento'
+            Menus.enviar_msg_Cabeamento_estruturado(wa_id)
+            Menus.enviar_msg_exemplo(wa_id)
             registrar_mensagem(wa_id, nome, button_reply_id, opcao_menu)
 
         print("_________________________________________")
@@ -220,12 +322,13 @@ def receive_messages():
             if text:
                 registrar_mensagem(numero_user, name, text, opcao_menu)
 
-            if MenuOptions.SUPORTE4 and palavra_chave in text:
-                opcao_menu = "OUTROS"
+            if palavra_chave in text:
                 detalhes = text
-                enviar_msg(numero_suporte, f"\nO cliente: {name}\n\nnumero: {numero_user}\n\nEsta requisitando suporte\n\naqui estao os detalhes: {detalhes}\n")
-                enviar_msg(numero_user, "Solicitaçao enviada para a equipe de suporte, aguarde o contato da nossa equipe, se precisar de mais alguma coisa pode contar com a gente")
+                enviar_msg(numero_para_enviar, f"\nO cliente: {name}\n\nnumero: {numero_user}\n\nEsta requisitando {opcao_menu}\n\naqui estao os detalhes:\n\n {detalhes}\n\n")
+                Menus.enviar_contato(numero_para_enviar, name, numero_user)
+                enviar_msg(numero_user, f"Solicitaçao enviada para a equipe do {equipe}, aguarde o contato da nossa equipe, se precisar de mais alguma coisa pode contar com a gente")
                 menu_atual = MenuOptions.INICIAL
+
 
         print("_________________________________________")
         print("Recebido POST do Usuario:")
